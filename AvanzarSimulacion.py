d07d36c9542a9d376139346b007c48543658f709
Juan Biancuzzo <72982017+JuanBiancuzzo@users.noreply.github.com>
2023-03-29 23:16:12 -0300

    Simplificando el metodo
    Haciendo que todas las funciones esten en el mismo archivo, no se estan eliminando los archivos sino que los estoy moviendo al archivo de ecuacion de calor

--- AvanzarSimulacion.py
+++
@@ -1,4 +0,0 @@
-import numpy as np
-
-def AvanzarSimulacion(campoActual, dt, coeficiente, kernel):
-    return campoActual / (1 - dt * coeficiente * kernel)
--- Transformaciones.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-def TransformacionDelCampo(campoActual):
-    return np.fft.fft(campoActual)
-
-def DestransformacionDelCampo(campoTransformado):
-    return np.fft.ifft(campoTransformado)
